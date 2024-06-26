f939e9fb31ef16bda68657745ae68721899a95cd
XianPearl <101512958+XianPearl@users.noreply.github.com>
2024-06-26 17:43:32 +0530

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "languagetranslatorfdbef5b3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "translateText2b0f69da": {
+      "region": "string",
+      "sourceLang": "string",
+      "targetLang": "string"
+    }
+  }
+}
